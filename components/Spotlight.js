c61646f2cbdc9287e5dc88749fa17883492e4659
Christof Röhl <userpattern@gmail.com>
2024-03-04 10:17:34 +0100

    user-story 2

---
+++ components/Spotlight.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Spotlight({ image, artist }) {
+  return (
+    <>
+      <article>
+        <h2>Spotlight Art Piece</h2>
+        <Image
+          src={image}
+          height={600}
+          width={400}
+          alt={`A picture by ${artist}`}
+        />
+        <p>{artist}</p>
+      </article>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 import ArtPieces from "@/components/ArtPieces";
+import Spotlight from "@/components/Spotlight";
 const fetcher = (url) => fetch(URL).then((response) => response.json());
 
 const URL = `https://example-apis.vercel.app/api/art`;
@@ -10,10 +11,18 @@ export default function HomePage() {
 
   if (error) return <div>{error.message}</div>;
   if (isLoading) return <div>loading...</div>;
-
+  function getRandomPiece(length) {
+    return Math.floor(Math.random() * length);
+  }
+  const randomPiece = pieces[getRandomPiece(pieces.length)];
+  console.log(randomPiece);
   return (
     <div>
       <h1>MEGA ART GALLERY APP 4000</h1>
+      <Spotlight
+        artist={randomPiece.artist}
+        image={randomPiece.imageSource}
+      ></Spotlight>
       <ArtPieces pieces={pieces}></ArtPieces>
     </div>
   );
